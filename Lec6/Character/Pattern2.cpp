6fceaf551ddf001603e3342ffb4a831fcd1516d1
Anamika Sharma <140599095+anamikasharma7@users.noreply.github.com>
2025-12-23 13:57:45 +0530

    Pattern Printing:1

---
+++ Lec6/Character/Pattern2.cpp
@@ -0,0 +1,22 @@
+/*
+a b c d e
+a b c d e
+a b c d e
+a b c d e
+a b c d e
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    char row,col;
+    for(row=1;row<=5;row=row+1)
+    {
+        for(col='a';col<='e';col=col+1)
+        {
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
---
+++ Lec6/Integer/Pattern2.cpp
@@ -0,0 +1,20 @@
+/*
+10 10 10 10 10
+10 10 10 10 10
+10 10 10 10 10
+10 10 10 10 10
+*/
+
+#include<iostream>
+using namespace std;
+int main(){
+    int col,row;
+    for(row=1;row<=4;row=row+1)
+    {
+        for(col=1;col<=5;col=col+1)
+        {
+            cout<<"10"<<" ";
+        }
+        cout<<endl;
+    }
+    }
